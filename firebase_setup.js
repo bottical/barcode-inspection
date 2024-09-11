97ef8a442c1435a32c97a06a9b9ec0e695b9872a
bottical <129402269+bottical@users.noreply.github.com>
2024-09-11 19:40:33 +0900

    Update firebase_setup.js

--- firebase_setup.js
+++ firebase_setup.js
@@ -1,13 +1,12 @@
 // Firebaseの初期化コード
 // Firebaseコンソールで取得した設定を使用してfirebaseを初期化します
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyDNLyIJYdnKFPZxouhcUZPQf_UwN0afSY4",
+  authDomain: "barcode-inspection.firebaseapp.com",
+  projectId: "barcode-inspection",
+  storageBucket: "barcode-inspection.appspot.com",
+  messagingSenderId: "451122231472",
+  appId: "1:451122231472:web:fb7ded68d1b25c33166375"
 };
 
 // Firebase初期化
